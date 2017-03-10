a6a6041d35e223c42d2cbdf77d4084d5d6a1230b
jinhui.lu <jinhui.lu@tcl.com>
2017-03-10 20:40:11 +0800

    语言列表添加tr

--- common/js/base.js
+++ common/js/base.js
@@ -26,7 +26,7 @@ var langListConfig = {
   "sk": "slovenčina",
   "sl": "slovenski",
   "sv": "svenska",
-  //"tr": "Türkçe",
+  "tr": "Türkçe",
   "fi":'Finnish',
   "et": "Estonian",
   "zh_CN": "简体中文"
